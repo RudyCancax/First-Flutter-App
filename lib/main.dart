7d4ca0c8ff5546f07d5970b52e2a9105ae2a72e1
rudycancax <rudycancax@gmail.com>
2024-02-15 12:25:56 -0600

    Feat: HomeScreen
    A new file home_screen.dart has been added. The HomeScreen class is imported into main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_flutter_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Center(child: Text("Hola mundo!!")),
-    );
+    return const HomeScreen();
   }
 }
