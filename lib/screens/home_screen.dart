2c5f1e6cdaa5564613e2efec27fd98b424db2917
rudycancax <rudycancax@gmail.com>
2024-02-15 13:53:05 -0600

    Feat: Scafold, column and text styles

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,9 +5,26 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Center(child: Text("Hola mundosssssss")),
+    var counter = 0;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Home Screen")),
+        backgroundColor: Colors.blueGrey[600],
+        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 30),
+        elevation: 10.0,
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Clicks Counter:",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+          Text(
+            "$counter",
+            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+          )
+        ],
+      )),
     );
   }
 }
